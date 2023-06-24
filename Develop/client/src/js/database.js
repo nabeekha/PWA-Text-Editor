5b4670339258ed1ebfc7068de820f534decf2da7
Nabeel Khan <nabeekha@umich.edu>
2023-06-24 14:10:18 -0400

    commit with database.js file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,14 +14,23 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  const DB = await openDB("jate", 1);
+  const syntax = DB.transaction("jate", "readwrite");
+  const store = syntax.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
   const result = await request
   console.error("Information has been uploaded to the database", result)
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-
   console.log("Information has been uploaded to the database", result)
+  const DB = await openDB("jate", 1);
+  const syntax = DB.transaction("jate", "readonly");
+  const store = syntax.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("Information has been uploaded to the database", result);
   return result.value
 }
 
